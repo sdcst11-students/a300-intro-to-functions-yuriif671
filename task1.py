99db57aaa2f480408d8212131bbce5106c5cf7fa
yuriif671 <yuriif767@deltalearns.ca>
2023-10-24 13:02:59 -0700

    Done

--- task1.py
+++ task1.py
@@ -1,6 +1,19 @@
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you?")
 
 def C():
+    print("Hi")
+
+x = str(input("What function do u wanna run?: "))
+
+if x == 'A':
+    A()
+elif x == 'B':
+    B()
+elif x == 'C':
+    C()
+else:
+    print("Nuh-uh!")
--- task2.py
+++ task2.py
@@ -8,3 +8,32 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+import random
+
+def title():
+    print('\033[1m' + '=' * 8 + " Welcome! " + '=' * 8 +  '\033[0m')
+    print("Guess the number between 1 and 100. \nEnter the number in the field below.\n")
+
+def game():
+    n = random.randint(1, 100)
+    attempt = 1
+
+    while True:
+        a = int(input("Gimme a number: "))
+
+        if a == n:
+            print('\033[1m' + f"\nYou won! The number was {n}. It took you {attempt} attempts." + '\033[0m')
+            break
+        elif a < n:
+            print("Too low")
+            attempt += 1
+        elif a > n:
+            print("Too high")
+            attempt += 1
+
+def main():
+    title()
+    game()
+
+main()
